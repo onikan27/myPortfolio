312ab1e64685bc1f11ea8d5ef40cb1840f838f26
onikan27 <ude0721juji@icloud.com>
2021-05-01 20:09:02 +0900

    feat: add prettir eslint

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import styles from 'styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      {/* ... */}
+    </div>
+  )
+}
+
+export default Home
