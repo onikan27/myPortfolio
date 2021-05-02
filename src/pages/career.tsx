1ca06f33f0ceff924d3bedf3099d44413c9af073
onikan27 <ude0721juji@icloud.com>
2021-05-02 23:18:02 +0900

    feat: add careet page

--- src/pages/career.tsx
+++ src/pages/career.tsx
@@ -1,7 +1,27 @@
+import { Center, Image } from '@chakra-ui/react'
 import { NextPage } from 'next'
+import Header from 'src/components/atoms/Header'
+import Text from 'src/components/atoms/Text'
+import Title from 'src/components/atoms/Title'
+import MainLayout from 'src/components/layout/MainLayout'
+import QuoteBox from 'src/components/molecules/QuoteBox'
 
 const career: NextPage = () => {
-  return <p>キャリアページ</p>
+  return (
+    <MainLayout>
+      <Center mb="64px">
+        <Image src="/carrier.svg" w="440px" h="280px" />
+      </Center>
+      <Title title="My Career" />
+      <Header>ヘルスケアITベンチャー</Header>
+      <QuoteBox>
+        <p> 規模感：50人</p>
+        <p> 勤務形態：アルバイト</p>
+        <p> エンジニア数：20人弱</p>
+      </QuoteBox>
+      <Text>学生アルバイトとして</Text>
+    </MainLayout>
+  )
 }
 
 export default career
