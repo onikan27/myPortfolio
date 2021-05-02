beb6e44c8e0b5d24455792ddf78321b193399b9b
onikan27 <ude0721juji@icloud.com>
2021-05-02 23:18:22 +0900

    feat: add text component

---
+++ src/components/atoms/Text.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react'
+
+const Text: React.FC = ({ children }) => {
+  return (
+    <Box mt="36px" mb="36px">
+      {children}
+    </Box>
+  )
+}
+
+export default Text
