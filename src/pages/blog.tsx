5813ebe33886c90f0efee69d22d66bf560f14406
onikan27 <ude0721juji@icloud.com>
2021-05-05 14:05:03 +0900

    feat: Add article page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,7 +1,8 @@
-import { Box, Center, Image } from '@chakra-ui/react'
+import { Box, Center, Flex, Image } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import Title from 'src/components/atoms/Title'
 import MainLayout from 'src/components/layout/MainLayout'
+import Article from 'src/components/molecules/Article'
 
 const career: NextPage = () => {
   return (
@@ -12,6 +13,33 @@ const career: NextPage = () => {
             <Image src="/blog.svg" w="440px" h="280px" />
           </Center>
           <Title>Blog</Title>
+          <Flex
+            justifyContent="space-between"
+            mt="36px"
+            flexWrap="wrap"
+            gridTemplateColumns="repeat(2,1fr)"
+            gridGap="40px"
+          >
+            <Article
+              title="【Next.js】CSR,SSG,SSR,ISRがあやふやな人へざっくり解説する"
+              url="https://zenn.dev/akino/articles/78479998efef55"
+              platform="zenn"
+              release_date="2021.01.21"
+            />
+            <Article
+              title="【Rails+Fargate】開発環境のDocker化+Fargateデプロイチュートリアル"
+              url="https://zenn.dev/akino/books/629502485ddb14"
+              platform="zenn"
+              release_date="2021.05.02"
+            />
+            <Article
+              title="【Git-Flow+PR】プロジェクトをGit-Flowに移行してみた。"
+              url="https://zenn.dev/akino/articles/1dcecd60009dbb"
+              platform="zenn"
+              release_date="2021.03.17"
+            />
+            {/* <Article title="" url="" platform="zenn" release_date=""/> */}
+          </Flex>
         </MainLayout>
       </Box>
     </>
