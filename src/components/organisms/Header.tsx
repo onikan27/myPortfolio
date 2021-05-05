22a58cfd78c780073a30659c16d0d2461bee1f54
onikan27 <ude0721juji@icloud.com>
2021-05-05 15:39:26 +0900

    fix: Next link component

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,33 +1,42 @@
-import { Flex, Link, Text } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 
 const Header: React.FC = () => {
   return (
     <Flex h="64px" mr="24px" ml="24px" justifyContent="space-between">
       <Flex alignItems="center">
-        <Link href="/" style={{ textDecoration: 'none' }}>
-          <Text fontSize="30px" fontWeight="700">
-            Akino
-          </Text>
-        </Link>
+        <Box cursor="pointer">
+          <Link href="/">
+            <Text fontSize="30px" fontWeight="700">
+              Akino
+            </Text>
+          </Link>
+        </Box>
       </Flex>
       <Flex w="230px" alignItems="center" justifyContent="space-between">
-        <Link href="/" style={{ textDecoration: 'none' }}>
-          <Text fontSize="18px" fontWeight="700">
-            About
-          </Text>
-        </Link>
+        <Box cursor="pointer">
+          <Link href="/">
+            <Text fontSize="18px" fontWeight="700">
+              About
+            </Text>
+          </Link>
+        </Box>
 
-        <Link href="/career" style={{ textDecoration: 'none' }}>
-          <Text fontSize="18px" fontWeight="700">
-            Career&amp;Skill
-          </Text>
-        </Link>
+        <Box cursor="pointer">
+          <Link href="/career">
+            <Text fontSize="18px" fontWeight="700">
+              Career&amp;Skill
+            </Text>
+          </Link>
+        </Box>
 
-        <Link href="/blog" style={{ textDecoration: 'none' }}>
-          <Text fontSize="18px" fontWeight="700">
-            Blog
-          </Text>
-        </Link>
+        <Box cursor="pointer">
+          <Link href="/blog">
+            <Text fontSize="18px" fontWeight="700">
+              Blog
+            </Text>
+          </Link>
+        </Box>
       </Flex>
     </Flex>
   )
